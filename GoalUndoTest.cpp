f0b1cefed9ef4a7646c70ed2f31d9624afbfe9ee
Rohan Chordiya <rchordiya@mail.csuchico.edu>
2016-05-15 22:49:47 -0700

    Test cases for addOperation function

--- GoalUndoTest.cpp
+++ GoalUndoTest.cpp
@@ -4,7 +4,9 @@
 
 #include <gtest/gtest.h>
 #include "GoalUndo.h"
- 
+#include<string>
+using namespace std;
+
 class GoalUndoTest : public ::testing::Test
 {
 	protected:
@@ -17,4 +19,30 @@ class GoalUndoTest : public ::testing::Test
 TEST(GoalUndoTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
+}
+
+TEST(GoalUndoTest,addOperationwith_Noexistinggoal)
+{
+  GoalUndo x;
+  string str="sucess";
+	x.addOperation(str);
+  ASSERT_EQ(str,x.getGoal());
+}
+
+TEST(GoalUndoTest,addOperationwith_emptyString)
+{
+  GoalUndo x;
+  string str="";
+	x.addOperation(str);
+  ASSERT_EQ(str,x.getGoal());
+}
+
+TEST(GoalUndoTest,addOperationwith_existingGoal)
+{
+  GoalUndo x;
+  string str="Computer";
+  string str1="reliable";
+	x.addOperation(str);
+  x.addOperation(str1);
+  ASSERT_EQ(str,x.getGoal());
 }
